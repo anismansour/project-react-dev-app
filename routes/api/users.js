50d4ee89eba8ba78eff82cf87c487aecfd1b96ee
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2019-07-11 14:23:50 -0700

    express validator for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-// route GET API/USERS
-router.get("/", (req, res) => res.send("User route"));
+// route POST API/USERS
+//register user
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check("password", "enter a password with more than 6 characters").isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("User route");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 //connect db
 connectDB();
 
+//init  middleware
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API RUNNING"));
 
 //define routes
