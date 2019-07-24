7e30347440251b8a88866d33f8adee936f1ebeff
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2019-07-24 14:18:49 -0700

    delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -199,4 +199,38 @@ router.post(
   }
 );
 
+//delete   api/posts/:id/:comment_id
+// delete comment
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // remove comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    //check that comment exist
+    if (!comment) {
+      return res.status(404).json({ msg: "comment odes not exist" });
+    }
+    // check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "user not authorized" });
+    }
+    //get remove index
+
+    const removeIndex = post.comments
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
 module.exports = router;
